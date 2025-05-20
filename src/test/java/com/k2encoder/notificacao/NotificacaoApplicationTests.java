3a12501cae2ae93655902511e62b0deb7421f5ab
Klaus Santos <klausantos82@gmail.com>
2025-05-20 09:24:04 -0300

    Commit inicial

---
+++ src/test/java/com/k2encoder/notificacao/NotificacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.k2encoder.notificacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
